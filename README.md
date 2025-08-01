c6ee4b3b7c2a77686488464eddc3584ea140638e
Cristian <cpttrabajos@gmail.com>
2025-07-31 22:55:57 -0600

    created simple 2d character, with basic animation and movement

---
+++ README.md
@@ -0,0 +1,7 @@
+# Circle Survive
+
+
+This is a small solo project for the GTK Game Jam 2025, made by [Cristian Porras](https://cristianporras.com)
+
+
+- [Play the game](https://lagomentertainment.itch.io/circle-survive)
--- scenes/player/player.gd
+++ scenes/player/player.gd
@@ -1,5 +1,7 @@
 extends CharacterBody2D
 
+@export var speed = 400
+@onready var _animated_sprite = $AnimatedSprite2D
 
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
@@ -8,4 +10,23 @@ func _ready() -> void:
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta: float) -> void:
-	pass
+	var input_direction = get_input()
+	velocity = input_direction * speed
+	
+	move_and_slide()
+	animate(input_direction)
+	
+
+
+func get_input():
+	return Input.get_vector("left", "right", "up", "down")
+	
+	
+func animate(direction: Vector2):
+	if direction != Vector2.ZERO:
+		_animated_sprite.play("walk")
+		if direction.x != 0:
+			_animated_sprite.flip_h = direction.x < 0
+	else:
+		_animated_sprite.play("idle")
+		
